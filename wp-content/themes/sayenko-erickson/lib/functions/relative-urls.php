785207d16612f60aaa3fd870edb5613e6fc3b897
Kyle Rumble <kylerumble@gmail.com>
2020-04-05 16:12:13 -0700

    fix url

--- wp-content/themes/sayenko-erickson/lib/functions/relative-urls.php
+++ wp-content/themes/sayenko-erickson/lib/functions/relative-urls.php
@@ -1,8 +1,8 @@
 <?php
 
 add_filter ('wp_get_attachment_url', 'wp_make_theme_links_protocols_relative');
-function wp_make_theme_links_protocols_relative( $link ){
-    if( empty( get_option( 'autoptimize_cdn_url' ) ) {
+function wp_make_theme_links_protocols_relative( $link ) {
+    if( empty( get_option( 'autoptimize_cdn_url' ) ) ) {
         return $link;
     }
     //$url = 'https://39mzo2r2vex16vj0czuuv21m-wpengine.netdna-ssl.com';  
