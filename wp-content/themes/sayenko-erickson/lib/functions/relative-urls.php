b5d5994811e333b2d6f2ce5f5c0b39febe080c4f
Kyle Rumble <kylerumble@gmail.com>
2020-04-05 16:10:16 -0700

    fix cdn url

--- wp-content/themes/sayenko-erickson/lib/functions/relative-urls.php
+++ wp-content/themes/sayenko-erickson/lib/functions/relative-urls.php
@@ -2,7 +2,12 @@
 
 add_filter ('wp_get_attachment_url', 'wp_make_theme_links_protocols_relative');
 function wp_make_theme_links_protocols_relative( $link ){
-    $url = 'https://39mzo2r2vex16vj0czuuv21m-wpengine.netdna-ssl.com';  
+    if( empty( get_option( 'autoptimize_cdn_url' ) ) {
+        return;
+    }
+    //$url = 'https://39mzo2r2vex16vj0czuuv21m-wpengine.netdna-ssl.com';  
+    $url = get_option( 'autoptimize_cdn_url' );
+    $url = rtrim( $url, '/');
     $link = str_replace( site_url(), $url, $link );	
 	return $link;
 }
